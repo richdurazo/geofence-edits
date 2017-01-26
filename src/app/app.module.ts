7f5b553cf28a86cbb6d9cce16d69d4b823d3b8ba
tbarth17 <tommyhbrc@gmail.com>
2017-01-25 16:21:41 -0800

    BED-463 basic setup for router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { OverviewComponent } from './overview/overview.component';
+
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'overview', component: OverviewComponent },
+    { path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

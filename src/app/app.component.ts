fb0ba81cb3a0adc5a420975f3b417ac467ad5e2c
tbarth17 <tommyhbrc@gmail.com>
2017-01-26 13:57:31 -0800

    BED-449 adds support for scss in build system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Change another thing!';
+  title = 'Change another stuff!';
 }

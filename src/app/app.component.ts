e7c96af24a1321d4b0d578442586ff87cdcce532
tbarth17 <tommyhbrc@gmail.com>
2017-01-30 14:00:15 -0800

    BED-476 add basic logout method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    constructor (private auth: AuthService) {}
+
   title = 'Change another stuff!';
+
+  logout () {
+      this.auth.logout();
+  }
 }

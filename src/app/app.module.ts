3cf0fa12682a64128d0dbef62ea9456aedc1e378
tbarth17 <tommyhbrc@gmail.com>
2017-01-31 16:08:56 -0800

    BED-463 pulls router into separate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,46 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, Http, Headers, RequestOptions } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
 
-import { AuthHttp, AuthConfig } from 'angular2-jwt';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { OverviewComponent } from './overview/overview.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AuthService } from './auth/auth.service';
-import { AuthGuardService } from './auth/auth-guard.service';
-import { AuthApiService } from './auth/auth-api.service';
 import { UserApiService } from './shared/user-api.service';
-
-export function authHttpServiceFactory(http: Http, options: RequestOptions) {
-    return new AuthHttp(new AuthConfig({
-        tokenName: 'Authorization',
-        tokenGetter: (() => localStorage.getItem('id_token')),
-        globalHeaders: [{'Content-Type':'application/json'}],
-    }), http, options);
-}
-
-const appRoutes: Routes = [
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
-        path: 'overview',
-        component: OverviewComponent,
-        canActivate: [AuthGuardService]
-    },
-    {
-        path: '',
-        redirectTo: '/login',
-        pathMatch: 'full'
-    },
-    {
-        path: '**',
-        component: PageNotFoundComponent
-    }
-];
 
 @NgModule({
     declarations: [
@@ -51,24 +18,13 @@ const appRoutes: Routes = [
         PageNotFoundComponent
     ],
     imports: [
+        AppRoutingModule,
         BrowserModule,
         FormsModule,
-        HttpModule,
-        RouterModule.forRoot(appRoutes)
-    ],
-    exports: [
-        RouterModule
+        HttpModule
     ],
     providers: [
-        AuthService,
-        AuthGuardService,
-        AuthApiService,
-        UserApiService,
-        {
-            provide: AuthHttp,
-            useFactory: authHttpServiceFactory,
-            deps: [Http, RequestOptions]
-        }
+        UserApiService
     ],
     bootstrap: [AppComponent]
 })

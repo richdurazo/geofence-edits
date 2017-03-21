e1e77401e70c929806e4473cb83e607fad344393
tbarth17 <tommyhbrc@gmail.com>
2017-03-21 13:40:24 -0700

    BED-616 fixes template for nav

--- src/app/app-navigation/app-navigation.component.ts
+++ src/app/app-navigation/app-navigation.component.ts
@@ -2,19 +2,50 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './app-navigation.component.html',
-  styleUrls: ['./app-navigation.component.scss']
+    selector: 'app-navigation',
+    templateUrl: './app-navigation.component.html',
+    styleUrls: ['./app-navigation.component.scss']
 })
 export class AppNavigationComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService) { }
 
-  ngOnInit() {
-  }
+    routes = [
+        {
+            route: "/overview",
+            name: "Overview"
+        },
+        {
+            route: "/campaigns",
+            name: "Campaigns"
+        },
+        {
+            route: "/campaigns/create",
+            name: "Campaign Creator"
+        },
+        {
+            route: "/content",
+            name: "Content"
+        },
+        {
+            route: "/content/create",
+            name: "Content Creator"
+        },
+        {
+            route: "/triggers",
+            name: "Trigger"
+        },
+        {
+            route: "/triggers/create",
+            name: "Trigger Creator"
+        }
+    ];
 
-  logOut () {
-      this.authService.logOut();
-  }
+    ngOnInit() {
+    }
+
+    logOut () {
+        this.authService.logOut();
+    }
 
 }

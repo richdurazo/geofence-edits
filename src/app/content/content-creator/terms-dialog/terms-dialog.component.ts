19609f2837f8fbfd601be4c5bd411488435b39e1
tbarth17 <tommyhbrc@gmail.com>
2017-04-14 11:11:40 -0700

    fixes minor bugs

--- src/app/content/content-creator/terms-dialog/terms-dialog.component.ts
+++ src/app/content/content-creator/terms-dialog/terms-dialog.component.ts
@@ -2,26 +2,26 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
 
 @Component({
-  selector: 'app-terms-dialog',
-  templateUrl: './terms-dialog.component.html',
-  styleUrls: ['./terms-dialog.component.scss']
+    selector: 'app-terms-dialog',
+    templateUrl: './terms-dialog.component.html',
+    styleUrls: ['./terms-dialog.component.scss']
 })
 export class TermsDialogComponent implements OnInit {
 
-    terms: any;
+    model: any;
 
-  constructor(public dialogRef: MdDialogRef<TermsDialogComponent>, @Inject(MD_DIALOG_DATA) private data: any) {
-      this.data = {
-          terms: data
-      };
-   }
+    constructor(public dialogRef: MdDialogRef<TermsDialogComponent>, @Inject(MD_DIALOG_DATA) private data: any) {
+        this.model = {
+            terms: this.data
+        };
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  public submitForm (form) {
-      if (form.valid) {
-          this.dialogRef.close(this.data.terms);
-      }
-  }
+    public submitForm (form) {
+        if (form.valid) {
+            this.dialogRef.close(this.model.terms);
+        }
+    }
 }

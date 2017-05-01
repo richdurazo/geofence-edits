14e225117dcbf4ec8ccebef7ce7ac91f7bc36e61
tbarth17 <tommyhbrc@gmail.com>
2017-05-01 15:18:55 -0700

    fixes broken unit test

--- src/app/content/content-group/content-group.component.spec.ts
+++ src/app/content/content-group/content-group.component.spec.ts
@@ -1,25 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { TriggerApiService } from '../../triggers/shared/trigger-api.service';
+import { TriggerApiMockService } from '../../mocks/triggers/trigger-api-mock.service';
+import { ContentApiService } from '../shared/content-api.service';
+import { ContentApiMockService } from '../../mocks/content/content-api-mock.service';
 
 import { ContentGroupComponent } from './content-group.component';
 
 describe('ContentGroupComponent', () => {
-  let component: ContentGroupComponent;
-  let fixture: ComponentFixture<ContentGroupComponent>;
+    let component: ContentGroupComponent;
+    let fixture: ComponentFixture<ContentGroupComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ContentGroupComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ ContentGroupComponent ],
+            imports: [
+                FormsModule
+            ],
+            schemas: [
+                CUSTOM_ELEMENTS_SCHEMA
+            ],
+            providers: [
+                { provide: ContentApiService, useClass: ContentApiMockService },
+                { provide: TriggerApiService, useClass: TriggerApiMockService }
+            ]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ContentGroupComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ContentGroupComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

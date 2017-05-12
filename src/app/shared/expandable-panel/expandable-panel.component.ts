cd995cd95570ad87a6ac9060f9685b3632e32467
tbarth17 <tommyhbrc@gmail.com>
2017-05-12 11:10:04 -0700

    minor edits

--- src/app/shared/expandable-panel/expandable-panel.component.ts
+++ src/app/shared/expandable-panel/expandable-panel.component.ts
@@ -6,17 +6,6 @@ import { trigger, state, animate, transition, style } from '@angular/animations'
   templateUrl: './expandable-panel.component.html',
   styleUrls: ['./expandable-panel.component.scss'],
   animations: [
-    //   trigger('headerChange', [
-    //       state('true' ,
-    //           style({ 'border-width': '0' }),
-    //       ),
-    //       state('false',
-    //           style({ 'border-width': '1px' })
-    //       ),
-    //       transition('void => *', style({ 'border-bottom': '0' })
-    //       ),
-    //       transition('* => *', animate('.25s ease-in')),
-    //   ]),
       trigger('expandChange', [
           state('true' ,
               style({ height: '0', display : 'none' }),

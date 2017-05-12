138704cfa38213bd31176702d2a3fbb5dfa65600
tbarth17 <tommyhbrc@gmail.com>
2017-05-11 18:02:34 -0700

    fixes unit tests

--- src/app/shared/selectable-list/selectable-list-item/selectable-list-item.component.spec.ts
+++ src/app/shared/selectable-list/selectable-list-item/selectable-list-item.component.spec.ts
@@ -1,35 +1,45 @@
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { SelectableListItemComponent } from './selectable-list-item.component';
 
+@Component({
+    selector: 'test-component-wrapper',
+    template: '<app-selectable-list-item [item]="content"></app-selectable-list-item>'
+})
+class TestComponentWrapper {
+    content = { id: 1, name: 'foo' }
+}
+
 describe('SelectableListItemComponent', () => {
-  let component: SelectableListItemComponent;
-  let fixture: ComponentFixture<SelectableListItemComponent>;
+    let component: SelectableListItemComponent;
+    let fixture: ComponentFixture<TestComponentWrapper>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-          SelectableListItemComponent
-      ],
-      schemas: [
-          CUSTOM_ELEMENTS_SCHEMA
-      ],
-      imports: [
-        RouterTestingModule
-      ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                SelectableListItemComponent,
+                TestComponentWrapper
+            ],
+            schemas: [
+                CUSTOM_ELEMENTS_SCHEMA
+            ],
+            imports: [
+                RouterTestingModule
+            ]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SelectableListItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestComponentWrapper);
+        component = fixture.debugElement.children[0].componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

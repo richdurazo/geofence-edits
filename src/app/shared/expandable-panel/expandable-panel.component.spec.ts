fc7dc70094805b10d11148720a23b9ecbf695120
tbarth17 <tommyhbrc@gmail.com>
2017-05-23 17:10:04 -0700

    temporary mocks for failing tests

--- src/app/shared/expandable-panel/expandable-panel.component.spec.ts
+++ src/app/shared/expandable-panel/expandable-panel.component.spec.ts
@@ -1,9 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+import { ContentApiService } from '../../content/shared/content-api.service';
+import { ContentApiMockService } from '../../mocks/content/content-api-mock.service';
 
 import { ExpandablePanelComponent } from './expandable-panel.component';
 
@@ -20,8 +24,12 @@ describe('ExpandablePanelComponent', () => {
       imports: [
           BrowserAnimationsModule,
           ReactiveFormsModule,
+          FormsModule,
           FlexLayoutModule,
           MaterialModule
+      ],
+      providers: [
+          { provide: ContentApiService, useClass: ContentApiMockService }
       ]
     })
     .compileComponents();
@@ -30,6 +38,8 @@ describe('ExpandablePanelComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ExpandablePanelComponent);
     component = fixture.componentInstance;
+    // quick and dirty mock of @Input
+    component.item = {name: 'stuff'};
     fixture.detectChanges();
   });
 

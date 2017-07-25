051cf6a5eb96e5cdd6992317bd4ea1146b1e4a05
Richard Durazo <rcdurazo@gmail.com>
2017-07-25 10:33:15 -0700

    BED-647: handle login

--- src/app/auth/auth-api.service.ts
+++ src/app/auth/auth-api.service.ts
@@ -5,6 +5,8 @@ import { AppSettings } from '../app-settings';
 
 @Injectable()
 export class AuthApiService {
+  appApi = JSON.stringify({"api_key":"b5ff4ab0-416d-11e7-80a2-e14a8bff1f26","carrier_name":"Verizon Wireless","device_identifier":"HT69J0205464","device_manufacturer":"Google","device_model":"Pixel XL","locale":"en_US","os_name":"android","os_version":"25","sdk_version":"1.0","uuid":"4669fc20-416e-11e7-a3c8-c3873b711234"});
+
 
   constructor(private http: Http) { }
 
@@ -14,6 +16,7 @@ export class AuthApiService {
       let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
       let options = new RequestOptions({ headers: headers });
       let params: URLSearchParams = new URLSearchParams();
+      headers.append('device', `${this.appApi}`);
 
       params.set("email", credentials.email);
       params.set("password", credentials.password);
